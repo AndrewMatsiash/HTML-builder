11d7cef7beb3c846c98c7bd65d2045e121570a50
AndrewMatsiash <rabotatnadomne@gmail.com>
2022-05-23 23:46:46 +0300

    feat:done task 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+let path = require('path');
+
+const projectDistStyleCss = path.join(__dirname, 'project-dist', 'bundle.css')
+
+const writeStream = fs.createWriteStream(projectDistStyleCss)
+
+writeStream.on('data', (chunk) => {
+  if (err) throw err;
+})
+
+
+fs.readdir(path.resolve(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
+  if (err) throw err; // не прочитать содержимое папки
+
+  files.filter(element => element.isFile()).filter((el) => path.extname(el.name) === '.css')
+
+  for (let i = 0; i < files.length; i++) {
+
+    let readFile = fs.createReadStream(path.resolve(__dirname, 'styles', files[i].name))
+
+    readFile.on('data', (chunk) => {
+      if (err) throw err;
+      writeStream.write(chunk)
+    })
+
+  }
+
+});
