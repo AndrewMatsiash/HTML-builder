49784e463be639465fb3f2b04354e81789d89dd2
AndrewMatsiash <rabotatnadomne@gmail.com>
2022-05-23 23:44:08 +0300

    feat:done task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,36 @@
-
 let fs = require('fs');
 let path = require('path');
 
-fs.readdir(path.resolve(__dirname, 'secret-folder'), { withFileTypes: true }, (err, files) => {
-  if (err) throw err;
 
-  let filteredFile = files.filter(element => element.isFile())
-
-
-  for (let i = 0; i < filteredFile.length; i++) {
-    let FileName = path.basename(filteredFile[i].name, path.extname(filteredFile[i].name))
-    let expansion = path.extname(filteredFile[i].name).slice(1)
-    fs.stat(path.resolve(__dirname, 'secret-folder', filteredFile[i].name), (error, stats) => {
-      if (error) {
-        console.log(error);
-      }
-      else {
-        let result = [FileName, expansion, stats.size.toString()]
-        result = result.join('-')
-        console.log(result);
-      }
-    })
+fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force: true }, (err) => {
+  if (err) {
+    throw err.message
   }
+  fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, (err) => {
+    if (err) {
+      throw err
+    } // не удалось создать папку
+    console.log('Папка files-copy успешно создана');
+  });
+
+  fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+    if (err) throw err; // не прочитать содержимое папки
+    files.forEach(
+      (el) => {
+        fs.readFile(path.join(__dirname, 'files-copy', el), "utf8", function (error, data) {
+          if (err) {
+            throw err
+          }
+        })
+      })
+    for (let i = 0; i < files.length; i++) {
+      fs.copyFile(path.resolve(__dirname, 'files', files[i]), path.resolve(__dirname, 'files-copy', files[i]), err => {
+        if (err) throw err; // не удалось скопировать файл
+        console.log(`${files[i]} файл успешно скопирован`);
+      });
+    }
+
+  });
 })
+
 
